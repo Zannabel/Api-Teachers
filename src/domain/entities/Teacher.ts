9f772ca6b245467d360b878ead185b62776da202
Zannabel <zuluagaisabel8404@outlook.com>
2023-05-15 21:28:19 -0500

    Se crea la capa de dominio y el repositorio con el método de crear

--- src/domain/entities/Teacher.ts
+++ src/domain/entities/Teacher.ts
@@ -1,1 +1,12 @@
- 
+ export class Teacher {
+
+    constructor(
+        public name: string,
+        public description: string,
+        public email: string,
+        public birthday: Date,
+        public id?: string
+    ) {
+
+    }
+ }
